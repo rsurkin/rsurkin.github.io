4ce05727dbaa012b6be96ca1cd685a28d8603159
Rashad Surkin <r.surkin@gmail.com>
2019-03-24 14:00:11 +0300

    start new game shuffle

--- src/Game15/reducers/tiles.js
+++ src/Game15/reducers/tiles.js
@@ -1,4 +1,11 @@
-
+const createNewGame = () => {
+  return [
+    ...Array(16).keys()
+  ]
+    .sort(() => Math.random() - 0.5)
+    .sort((a, b) => b === 0 ? -1 : 0)
+  ;
+};
 
 export default (state = [], action) => {
   switch (action.type) {
@@ -12,8 +19,7 @@ export default (state = [], action) => {
         // }
       ];
     case 'NEW_GAME':
-      const tiles = [...Array(16).keys()];
-      return tiles;
+      return createNewGame();
     default:
       return state;
   }
